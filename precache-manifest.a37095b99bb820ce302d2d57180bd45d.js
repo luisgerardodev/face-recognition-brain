385a48628fb53fa6243064a7e69198f850305162
Luis Manuel Gerardo Morillo <lgerardo@mopc.gob.do>
2020-01-08 17:00:34 -0400

    Updates

---
+++ precache-manifest.a37095b99bb820ce302d2d57180bd45d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f2c80a30d1d393750fe9baa972b510",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "0d0481151e3dedbd590e",
+    "url": "/face-recognition-brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "99552fd6e2acd0aa4852",
+    "url": "/face-recognition-brain/static/css/main.293d8f70.chunk.css"
+  },
+  {
+    "revision": "0d0481151e3dedbd590e",
+    "url": "/face-recognition-brain/static/js/2.f192ede6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/face-recognition-brain/static/js/2.f192ede6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99552fd6e2acd0aa4852",
+    "url": "/face-recognition-brain/static/js/main.128e1f16.chunk.js"
+  },
+  {
+    "revision": "48fe6b820b4de8bfe933",
+    "url": "/face-recognition-brain/static/js/runtime-main.5177fc43.js"
+  }
+]);
