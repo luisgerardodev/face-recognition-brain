cea0778115adef3075f55b9431263fa5be606212
Luis Manuel Gerardo Morillo <lgerardo@mopc.gob.do>
2020-01-09 12:48:19 -0400

    added sign in and register form

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Navigation = () => {
+const Navigation = ({ isSignedIn, onRouteChange }) => {
     return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+        isSignedIn
+        ? <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
         </nav>
-    );
+        : <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+            <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+        </nav>
+    )
+
 }
 
 export default Navigation;
